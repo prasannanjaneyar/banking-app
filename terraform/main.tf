f365c4afd3d92b8f8e1b7fa233284f1a2ce52c3f
Prasannanjaneya <Prasannanjaneya.r@gmail.com>
2025-11-23 18:37:22 +0100

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku                 = "Standard"
-  admin_enabled       = false
+  admin_enabled       = true
   
   tags = var.tags
 }
